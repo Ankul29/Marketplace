5a987b9f7a0c761553469f33f22ee2a02d732943
ankul <ankul@colexion.io>
2022-11-10 09:40:10 +0530

    first commit

---
+++ abc.sol
@@ -0,0 +1,13 @@
+`   // SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "https://github.com/solv-finance/erc-3525/blob/main/contracts/ERC3525Upgradeable.sol";
+
+import "https://github.com/solv-finance/erc-3525/blob/main/contracts/ERC3525MintableUpgradeable.sol";
+
+//import "https://github.com/solv-finance/erc-3525/blob/main/contracts/ERC3525SlotEnumerableUpgradeable.sol";
+contract Token3525 is ERC3525Upgradeable , ERC3525MintableUpgradeable    {
+
+
+}
